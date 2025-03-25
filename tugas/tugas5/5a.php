5d584961af80dcb337e9564fbf3086932a33ae1e
AndhikaAshari <andhikadimari02@gmail.com>
2025-03-25 13:59:20 +0700

    Memperbaiki tugas 5

--- tugas/tugas5/5a.php
+++ tugas/tugas5/5a.php
@@ -1,15 +1,75 @@
 <?php
 $mahasiswa = [
-    ["Sandhika Galih", "043040023", "Teknik Informatika", "sandhikagalih@unpas.ac.id"],
-    ["Doddy Ferdiansyah", "033040001", "Teknik Industri", "doddy@yahoo.com"],
-    ["Erik", "023040123", "Teknik Planologi", "erik@gmail.com"],
-    ["Andhika Ashari", "243040034", "Teknik Informatika", "andhikadimari02@gmail.com"],
-    ["Muhammad Nazriel Bintang Putra Darajat", "243040026", "Teknik Informatika", "nazriel@gmail.com"],
-    ["Dion Marshall Avalon Adhiseputro", "243040030", "Teknik Informatika", "dion@gmail.com"],
-    ["Dwi Dzaky Wibowo", "243040011", "Teknik Informatika", "dzaky@gmail.com"],
-    ["Ikhsan Ferdiansyah", "243040017", "Teknik Informatika", "ikhsan@gmail.com"],
-    ["Reiza Mohamad Aditia", "243040024", "Teknik Informatika", "reiza@gmail.com"],
-    ["Muhammad Sholeh Maulana", "243040035", "Teknik Informatika", "sholeh@gmail.com"],
+    [
+        "nama" => "Sandhika Galih",
+        "nrp" => "043040023",
+        "Jurusan" => "Teknik Informatika",
+        "Email" => "sandhikagalih@unpas.ac.id"
+    ],
+
+    [
+        "nama" => "Doddy Ferdiansyah",
+        "nrp" => "033040001",
+        "Jurusan" => "Teknik Industri",
+        "Email" => "doddy@yahoo.com"
+    ],
+
+    [
+        "nama" => "Erik",
+        "nrp" => "023040123",
+        "Jurusan" => "Teknik Planologi",
+        "Email" => "erik@gmail.com"
+    ],
+
+    [
+        "nama" => "Andhika Ashari",
+        "nrp" => "243040034",
+        "Jurusan" => "Teknik Informatika",
+        "Email" => "andhikadimari02@gmail.com"
+    ],
+
+    [
+        "nama" => "Muhammad Nazriel Bintang Putra Darajat",
+        "nrp" => "243040026",
+        "Jurusan" => "Teknik Informatika",
+        "Email" => "nazriel@gmail.com"
+    ],
+
+    [
+        "nama" => "Dion Marshall Avalon Adhiseputro",
+        "nrp" => "243040030",
+        "Jurusan" => "Teknik Informatika",
+        "Email" => "dion@gmail.com"
+    ],
+
+    [
+        "nama" => "Dwi Dzaky Wibowo",
+        "nrp" => "243040011",
+        "Jurusan" => "Teknik Informatika",
+        "Email" => "dzaky@gmail.com"
+    ],
+
+    [
+        "nama" => "Ikhsan Ferdiansyah",
+        "nrp" => "243040017",
+        "Jurusan" => "Teknik Informatika",
+        "Email" => "ikhsan@gmail.com"
+    ],
+
+    [
+        "nama" => "Reiza Mohamad Aditia",
+        "nrp" => "243040024",
+        "Jurusan" => "Teknik Informatika",
+        "Email" => "reiza@gmail.com"
+    ],
+
+    [
+        "nama" => "Muhammad Sholeh Maulana",
+        "nrp" => "243040035",
+        "Jurusan" => "Teknik Informatika",
+        "Email" => "sholeh@gmail.com"
+    ],
+
 ]
 ?>
 
@@ -24,10 +84,10 @@ $mahasiswa = [
     <h1>Daftar Mahasiswa</h1>
     <?php foreach ($mahasiswa as $mhs) : ?>
         <ul>
-            <li>Nama : <?= $mhs[0]; ?></li>
-            <li>NRP : <?= $mhs[1]; ?></li>
-            <li>Jurusan : <?= $mhs[2]; ?></li>
-            <li>Email : <?= $mhs[3]; ?></li>
+            <li>Nama : <?= $mhs["nama"]; ?></li>
+            <li>NRP : <?= $mhs["nrp"]; ?></li>
+            <li>Jurusan : <?= $mhs["Jurusan"]; ?></li>
+            <li>Email : <?= $mhs["Email"]; ?></li>
         </ul>
     <?php endforeach; ?>
 
